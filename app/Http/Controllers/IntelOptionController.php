5bcf313916eef6a79d87200fc7672b4d912c393d
coordproyectosticsocbp <Carolinesc123>
2021-03-29 17:59:03 -0500

    "Validaciones de campo no vacio, mayor o igual a 50"

--- app/Http/Controllers/IntelOptionController.php
+++ app/Http/Controllers/IntelOptionController.php
@@ -15,60 +15,84 @@ class IntelOptionController extends Controller
 
     public function getDataUsersPopulation(Request $request, $age)
     {
-        if ($request->hasHeader('X-Authorization')) {
+        if ($request->hasHeader('X-Authorization'))
+        {
 
-            $query = DB::connection('sqlsrv_hosv')
-                ->select(
-                    DB::raw("SELECT TOP(100) * FROM DATOS_DEMOGRAFICOS('$age')")
-                );
-
-            if (count($query ) > 0)
+            if ($age)
             {
 
-                $records = [];
-
-                foreach ($query as $item)
+                if ($age >= 50)
                 {
+                    $query = DB::connection('sqlsrv_hosv')
+                        ->select(
+                            DB::raw("SELECT TOP(100) * FROM DATOS_DEMOGRAFICOS('$age')")
+                        );
 
-                    $temp = array(
-                        'tipDoc' => $item->TIP_DOC,
-                        'documento' => $item->DOCUMENTO,
-                        'pNombre' => $item->PRIMER_NOMBRE,
-                        'sNombre' => $item->SEGUNDO_NOMBRE,
-                        'pApellido' => $item->PRIMER_APELLIDO,
-                        'sApellido' => $item->SEGUNDO_APELLIDO,
-                        'sexo' => $item->SEXO,
-                        'fNacimiento' => $item->FECHA_NACIMIFECHA_NACIMI,
-                        'empresa' => $item->EMPRESA,
-                        'regimen' => $item->REGIMEN,
-                        'categoria' => $item->CATEGORIA,
-                        'telefono1' => $item->TELEFONO1,
-                        'telefono2' => $item->TELEFONO2,
-                        'telefono3' => $item->TELEFONO3,
-                    );
+                    if (count($query ) > 0)
+                    {
 
-                    $records[] = $temp;
+                        $records = [];
+
+                        foreach ($query as $item)
+                        {
+
+                            $temp = array(
+                                'tipDoc' => $item->TIP_DOC,
+                                'documento' => $item->DOCUMENTO,
+                                'pNombre' => $item->PRIMER_NOMBRE,
+                                'sNombre' => $item->SEGUNDO_NOMBRE,
+                                'pApellido' => $item->PRIMER_APELLIDO,
+                                'sApellido' => $item->SEGUNDO_APELLIDO,
+                                'sexo' => $item->SEXO,
+                                'fNacimiento' => $item->FECHA_NACIMIFECHA_NACIMI,
+                                'empresa' => $item->EMPRESA,
+                                'regimen' => $item->REGIMEN,
+                                'categoria' => $item->CATEGORIA,
+                                'telefono1' => $item->TELEFONO1,
+                                'telefono2' => $item->TELEFONO2,
+                                'telefono3' => $item->TELEFONO3,
+                            );
+
+                            $records[] = $temp;
+                        }
+
+                        return response()->json([
+                            'msg' => 'Ok',
+                            'status' => 200,
+                            'data' => $records
+                        ], 200);
+
+                    } else {
+
+                        return response()->json([
+                            'msg' => 'No Hay Datos en la Respuesta',
+                            'status' => 204
+                        ], 204);
+
+                    }
+                } else {
+
+                    return response()->json([
+                        'msg' => 'La Edad Debe Ser Mayor o Igual a 50 Años',
+                        'status' => 200
+                    ], 200);
+
                 }
-
-                //return $temp;
-
-                return response()->json([
-                    'msg' => 'In',
-                    'data' => $records
-                ], 200);
 
             } else {
 
                 return response()->json([
-                   'msg' => 'No hay datos en la respuesta'
-                ], 204);
+                    'msg' => 'El Parametro Edad No Puede Estar Vacío',
+                    'status' => 200
+                ], 200);
 
             }
 
         } else {
 
             return response()->json([
-                'msg' => 'Acceso No autorizado'
+                'msg' => 'Acceso No Autorizado',
+                'status' => 401
             ], 401);
 
         }
